25a7213e371275e8ab7d29ea28e3c53fb9f9825c
mireu-lab <limmireu@kakao.com>
2022-06-24 14:23:33 +0000

    upload

--- App/ecocycling/qr.py
+++ App/ecocycling/qr.py
@@ -3,6 +3,7 @@ from pytz import timezone
 
 import qrcode
 import hashlib
+import requests
 import json
 
 with open("Data/Set.json", "r") as setdata:
@@ -11,20 +12,21 @@ with open("Data/Set.json", "r") as setdata:
 device_id = set_data["device"]["id"]
 device_zone = set_data["device"]["zone"]
 device_local = set_data["device"]["local"]
+device_ip = requests.get("http://ip.jsontest.com").json()["ip"]
 
 qr = qrcode.QRCode(
-    version=2,
+    version=4,
     error_correction=qrcode.constants.ERROR_CORRECT_M,
-    box_size=4,
+    box_size=20,
     border=1
 )
 
 def add():
     time = datetime.now(timezone(device_zone)).strftime('%Y/%m/%d-%H:%M:%S')
     
-    qr.add_data({"device_id" : device_id})
+    qr.add_data([device_ip])
     qr.make(fit=True)
     qr_img = qr.make_image(fill_color="black", back_color="white")
-    qr_img.save(f"App/static/{device_id}-{device_local}.png")
+    qr_img.save(f"App/static/main.png")
     
     print(f"QR CODR ADD CLRAR : {time}")
--- App/main.py
+++ App/main.py
@@ -1,8 +1,6 @@
 from ecocycling import qr
 
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, request, render_template, url_for, redirect, jsonify
 app = Flask(__name__)
 
 import json
@@ -13,12 +11,24 @@ with open("Data/Set.json", "r") as setdata:
 deviceid = set_data["device"]["id"]
 devicelocal = set_data["device"]["local"]
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def qr_web():
-    return render_template(
-        'main.html', 
-        image_file=f"{deviceid}-{devicelocal}.png"
-    )
+    if request.method == "GET":
+        if request.remote_addr == '127.0.0.1':
+            return render_template('main.html')
+        else:
+            return redirect('http://hana.mireu.xyz')
+
+@app.route('/singin', methods=["POST", "GET"])
+def singin():
+    if request.method == "POST":
+        input_data = request.get_json()
+        print(input_data["userid"], input_data["username"], input_data["userimage"])
+        return input_data["username"], input_data["userimage"]
+    else:
+        return redirect('http://hana.mireu.xyz')
+
+
 
 if __name__ == '__main__':
     qr.add()
