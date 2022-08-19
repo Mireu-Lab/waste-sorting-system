e52aa38ef80b3e3068e88672cfebb6d1115c8d86
mireu-labcon <alfmalfm1214@gmail.com>
2022-08-20 02:35:06 +0900

    설치 라이브러리 세팅, 리눅스 백그라운드 실행

--- Install/install.sh
+++ Install/install.sh
@@ -1,1 +1,16 @@
-pip3 install -r Install/requirements.txt
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install -r Install/requirements.txt
+
+sudo cp Install/Service/FrontWeb_service.service /etc/systemd/system/
+sudo cp Install/Service/serviceAPI_service.service /etc/systemd/system/
+sudo cp Install/Service/Setup_service.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable FrontWeb_service
+sudo systemctl enable serviceAPI_service
+sudo systemctl enable Setup_service
+
+sudo systemctl start FrontWeb_service
+sudo systemctl start serviceAPI_service
+sudo systemctl start Setup_service
