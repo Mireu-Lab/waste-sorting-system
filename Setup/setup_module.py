ae5772b6631fa7ae46e2547c8669ba7ccf6bfd3d
mireu-lab <limmireu@kakao.com>
2022-08-22 01:41:39 +0900

    버그 수정 및 에러 확인

--- Setup/setup_module.py
+++ Setup/setup_module.py
@@ -19,18 +19,21 @@ def system_setup():
     return "Complete instrument basic setup"
 
 def system_info_update(token=None, service_version=None, ai_version=None):
-    device_info = open("Data/Set.json", "a")
+    device_info_json = json.load(open("Data/Set.json", "r"))
 
-    if requests.get("http://ip.jsontest.com").json()["ip"] != device_info["device"]["ip"]:
-        device_info["device"]["ip"] = requests.get("http://ip.jsontest.com").json()["ip"]
+    save_json = open("Data/Set.json", "a")
+
+    if requests.get("http://ip.jsontest.com").json()["ip"] != device_info_json["device"]["ip"]:
+        device_info_json["device"]["ip"] = requests.get("http://ip.jsontest.com").json()["ip"]
     
     if token != None:
-        device_info["device"]["token"] = token
+        device_info_json["device"]["token"] = token
     
     if service_version != None:
-        device_info["version"]["service"] = ai_version
+        device_info_json["version"]["service"] = ai_version
 
     if ai_version != None:
-        device_info["version"]["ai"] = ai_version
+        device_info_json["version"]["ai"] = ai_version
 
-    json.dump(device_info, device_info)
+    json.dump(device_info_json, save_json, indent=4)
+    return "System Info Update processing complete"
